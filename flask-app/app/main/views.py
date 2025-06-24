5d578dca5c93ffc278ed91b616969423e214e783
Nick Cote <ncote@ucar.edu>
2025-06-24 10:33:37 -0600

    add in env variable

--- flask-app/app/main/views.py
+++ flask-app/app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template
 from app import app
+import os
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    welcome_message = os.getenv('WELCOME_MESSAGE', 'Welcome to the Flask Demo!')
+    return render_template('home.html', welcome_message=welcome_message)
 
 @app.route('/templates/navbar.html')
 def navbar():
